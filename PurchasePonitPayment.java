3ed9470ccebb6db6b7fcc596edacb314586aefbf
NaokiKaneko03 <c0a22046fe@edu.teu.ac.jp>
2023-12-07 14:21:55 +0900

    Step4 論理否定を無くし、読み易くする

--- PurchasePonitPayment.java
+++ PurchasePonitPayment.java
@@ -1,23 +1,24 @@
  import java.time.LocalDateTime;
+
 class PurchasePointPayment {
     final CustomerId customerId; // 購入者のID
     final ComicId comicId; // 購入するWebコミックのID
     final PurchasePoint consumptionPoint; // 購入で消費するポイント
     final LocalDateTime paymentDateTime; // 購入日時
     PurchasePointPayment(final Customer customer, final Comic comic) {
-        if (!customer.isEnabled()) {
+        if (customer.isDisabled()) {
             throw new IllegalArgumentException("有効な購入者ではありません。");
         }
-        if (!comic.isEnabled()) {
+        if (comic.isDisabled()) {
             throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
-        }
-        if (customer.possessionPoint.amount < comic.currentPurchasePoint.amount) {
-            throw new RuntimeException("所持ポイントが不足しています。");
+        }   if (customer.possessionPoint.amount < comic.currentPurchasePoint.amount) {
+            throw new RuntimeException("所持ポイントが不足しています。");   
         }
         customerId = customer.id;
         comicId = comic.id;
         consumptionPoint = comic.currentPurchasePoint;
         paymentDateTime = LocalDateTime.now();
+
     }
 }
 
